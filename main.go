952a846f162a370825ec0dd6c29b7b2c519bf687
IfedayoAwe <olalekanawe99@gmail.com>
2022-09-03 22:28:33 +0100

    open, read, close files

--- main.go
+++ main.go
@@ -62,3 +62,28 @@ package main
 // 		fmt.Println(v.key, "appears", v.val, "times")
 // 	}
 // }
+
+// open, read and close multiple files
+// import (
+// 	"fmt"
+// 	"io"
+// 	"os"
+// )
+
+// func main() {
+// 	for _, fname := range os.Args[1:] {
+// 		file, err := os.Open(fname)
+
+// 		if err != nil {
+// 			fmt.Fprintln(os.Stderr, err)
+// 			continue
+// 		}
+
+// 		if _, err := io.Copy(os.Stdout, file); err != nil {
+// 			fmt.Fprintln(os.Stderr, err)
+// 			continue
+// 		}
+
+// 		file.Close()
+// 	}
+// }
