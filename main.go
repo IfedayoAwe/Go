4dfee3869a36fafc2b2795a84303a7ec851fd551
IfedayoAwe <olalekanawe99@gmail.com>
2022-09-06 16:36:03 +0100

    closure

--- main.go
+++ main.go
@@ -160,3 +160,41 @@ package main
 // 	}
 
 // }
+
+// import (
+// 	"fmt"
+// )
+
+// func fib() func() int {
+// 	a, b := 0, 1
+
+// 	return func() int {
+// 		a, b = b, a+b
+// 		return b
+// 	}
+// }
+
+// func main() {
+// 	f, g := fib(), fib()
+// 	fmt.Println(f(), g())
+// }
+
+// Closure
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	s := make([]func(), 4)
+
+// 	for i := 0; i < 4; i++ {
+// 		i2 := i // closure capture
+// 		s[i] = func() {
+// 			fmt.Printf("%d @ %p\n", i2, &i2)
+// 		}
+// 	}
+
+// 	for i := 0; i < 4; i++ {
+// 		s[i]()
+// 	}
+// }
