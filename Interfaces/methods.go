ae92fe81898e8046983fb04b92fb6b7004f06dae
IfedayoAwe <olalekanawe99@gmail.com>
2022-09-18 18:31:43 +0100

    loop

--- Interfaces/methods.go
+++ Interfaces/methods.go
@@ -9,6 +9,8 @@ type Point struct {
 	x, y float64
 }
 
+type Path []Point
+
 type Line struct {
 	begin, end Point
 }
@@ -17,6 +19,15 @@ func (l Line) Distance() float64 {
 	return math.Hypot(l.end.x-l.begin.x, l.end.y-l.begin.y)
 }
 
+func (p Path) Distance() (sum float64) {
+	for i := 1; i < len(p); i++ {
+		sum += Line{p[i-1], p[i]}.Distance()
+	}
+
+	return sum
+}
+
 func main() {
 	fmt.Println(Line{Point{1, 2}, Point{4, 6}}.Distance())
+	fmt.Println(Path{{1, 1}, {5, 1}, {5, 4}, {1, 1}}.Distance())
 }
