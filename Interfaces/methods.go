c2b026bc9e8db181764ae7dd187fc5fc2361bb6f
IfedayoAwe <olalekanawe99@gmail.com>
2022-09-18 21:08:09 +0100

    composition introduction

--- Interfaces/methods.go
+++ Interfaces/methods.go
@@ -76,3 +76,31 @@ package main
 // 	fmt.Println(s2.Distance())
 // 	fmt.Println(Line{Point{1, 2}, Point{4, 6}}.ScaleBy(2).Distance())
 // }
+
+import (
+	"fmt"
+	"image/color"
+	"math"
+)
+
+type Point struct {
+	X, Y float64
+}
+
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+type ColoredPoint struct {
+	Point
+	Color color.RGBA
+}
+
+func main() {
+	p, q := Point{1, 1}, ColoredPoint{Point{5, 4}, color.RGBA{255, 0, 0, 255}}
+
+	l1 := q.Distance(p)
+	l2 := p.Distance(q.Point) // OK: but p.Distance(q) is NOT ALLOWED
+
+	fmt.Println(l1, l2)
+}
