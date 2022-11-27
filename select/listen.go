b953bbe5c9d825540596297a9dea0dad7969c6bc
IfedayoAwe <olalekanawe99@gmail.com>
2022-11-27 14:04:36 +0100

    stoppers

--- select/listen.go
+++ select/listen.go
@@ -1,31 +1,33 @@
+// Using select statements to listen to multiple channels at the same time
+
 package main
 
-import (
-	"log"
-	"time"
-)
+// import (
+// 	"log"
+// 	"time"
+// )
 
-func main() {
-	chans := []chan int{
-		make(chan int),
-		make(chan int),
-	}
+// func main() {
+// 	chans := []chan int{
+// 		make(chan int),
+// 		make(chan int),
+// 	}
 
-	for i := range chans {
-		go func(i int, ch chan<- int) {
-			for {
-				time.Sleep(time.Duration(i) * time.Second)
-				ch <- i
-			}
-		}(i+1, chans[i])
-	}
+// 	for i := range chans {
+// 		go func(i int, ch chan<- int) {
+// 			for {
+// 				time.Sleep(time.Duration(i) * time.Second)
+// 				ch <- i
+// 			}
+// 		}(i+1, chans[i])
+// 	}
 
-	for i := 0; i < 12; i++ {
-		select {
-		case m0 := <-chans[0]:
-			log.Println("received", m0)
-		case m1 := <-chans[1]:
-			log.Println("received", m1)
-		}
-	}
-}
+// 	for i := 0; i < 12; i++ {
+// 		select {
+// 		case m0 := <-chans[0]:
+// 			log.Println("received", m0)
+// 		case m1 := <-chans[1]:
+// 			log.Println("received", m1)
+// 		}
+// 	}
+// }
