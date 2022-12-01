7bda77abf3d9c24bd8922ecbd3801b59dc33a427
IfedayoAwe <olalekanawe99@gmail.com>
2022-12-01 01:22:25 +0100

    buffered channels

--- context/parallelget.go
+++ context/parallelget.go
@@ -1,61 +1,61 @@
 package main
 
-import (
-	"context"
-	"log"
-	"net/http"
-	"time"
-)
+// import (
+// 	"context"
+// 	"log"
+// 	"net/http"
+// 	"time"
+// )
 
-type result struct {
-	url     string
-	err     error
-	latency time.Duration
-}
+// type result struct {
+// 	url     string
+// 	err     error
+// 	latency time.Duration
+// }
 
-func get(ctx context.Context, url string, ch chan<- result) {
-	start := time.Now()
-	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+// func get(ctx context.Context, url string, ch chan<- result) {
+// 	start := time.Now()
+// 	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 
-	if resp, err := http.DefaultClient.Do(req); err != nil {
-		ch <- result{
-			url,
-			err,
-			0,
-		}
-	} else {
-		t := time.Since(start).Round(time.Millisecond)
-		ch <- result{
-			url,
-			nil,
-			t,
-		}
-		resp.Body.Close()
-	}
-}
+// 	if resp, err := http.DefaultClient.Do(req); err != nil {
+// 		ch <- result{
+// 			url,
+// 			err,
+// 			0,
+// 		}
+// 	} else {
+// 		t := time.Since(start).Round(time.Millisecond)
+// 		ch <- result{
+// 			url,
+// 			nil,
+// 			t,
+// 		}
+// 		resp.Body.Close()
+// 	}
+// }
 
-func main() {
-	results := make(chan result)
-	list := []string{
-		"https://amazon.com",
-		"https://google.com",
-		"https://nytimes.com",
-		"https://wsj.com",
-	}
+// func main() {
+// 	results := make(chan result)
+// 	list := []string{
+// 		"https://amazon.com",
+// 		"https://google.com",
+// 		"https://nytimes.com",
+// 		"https://wsj.com",
+// 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+// 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 
-	defer cancel()
+// 	defer cancel()
 
-	for _, url := range list {
-		go get(ctx, url, results)
-	}
+// 	for _, url := range list {
+// 		go get(ctx, url, results)
+// 	}
 
-	for range list {
-		if r := <-results; r.err != nil {
-			log.Printf("%-20s %s\n", r.url, r.err)
-		} else {
-			log.Printf("%-20s %s\n", r.url, r.latency)
-		}
-	}
-}
+// 	for range list {
+// 		if r := <-results; r.err != nil {
+// 			log.Printf("%-20s %s\n", r.url, r.err)
+// 		} else {
+// 			log.Printf("%-20s %s\n", r.url, r.latency)
+// 		}
+// 	}
+// }
