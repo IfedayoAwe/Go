dabee63218fe357dc558ebfa6a2f2dece9a47be6
IfedayoAwe <olalekanawe99@gmail.com>
2022-12-14 06:12:19 +0100

    incrementmutex

---
+++ Mutex/incrementmutex.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func do() int {
+	var m sync.Mutex
+
+	var n int64
+	var w sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		w.Add(1)
+
+		go func() {
+			m.Lock()
+			n++ // DATA RACE
+			m.Unlock()
+			w.Done()
+		}()
+	}
+
+	w.Wait()
+	return int(n)
+}
+
+func main() {
+	fmt.Println(do())
+}
