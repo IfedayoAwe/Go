55e3f0a6d112ec89021ab2bf12ebfef0a2d4ae63
IfedayoAwe <olalekanawe99@gmail.com>
2022-12-15 07:04:07 +0100

    safemap

--- Interfaces/store/safestore.go
+++ Interfaces/store/safestore.go
@@ -1,1 +1,131 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+type dollars float32 // Never use float type for money, use a package
+
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
+}
+
+// Fake database
+type database struct {
+	mu   sync.Mutex
+	data map[string]dollars
+}
+
+// // add the handlers
+func (db *database) list(w http.ResponseWriter, req *http.Request) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	for item, price := range db.data {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db *database) fetch(w http.ResponseWriter, req *http.Request) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	item := req.URL.Query().Get("item")
+
+	if _, ok := db.data[item]; !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+
+	fmt.Fprintf(w, "item %s has price %s\n", item, db.data[item])
+}
+func (db *database) create(w http.ResponseWriter, req *http.Request) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+
+	if _, ok := db.data[item]; ok {
+		w.WriteHeader(http.StatusBadRequest) // 404
+
+		fmt.Fprintf(w, "duplicate item: %q\n", item)
+		return
+	}
+
+	if f64, err := strconv.ParseFloat(price, 32); err != nil {
+		w.WriteHeader(http.StatusBadRequest) // 400
+
+		fmt.Fprintf(w, "invalid price: %q\n", price)
+	} else {
+		db.data[item] = dollars(f64)
+
+		fmt.Fprintf(w, "added %s with price %s\n", item, dollars(f64))
+	}
+}
+
+func (db *database) update(w http.ResponseWriter, req *http.Request) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+
+	if _, ok := db.data[item]; !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+
+	if f64, err := strconv.ParseFloat(price, 32); err != nil {
+		w.WriteHeader(http.StatusBadRequest) // 400
+
+		fmt.Fprintf(w, "invalid price: %q\n", price)
+	} else {
+		db.data[item] = dollars(f64)
+
+		fmt.Fprintf(w, "new price %s for %s\n", dollars(f64), item)
+	}
+}
+
+func (db *database) drop(w http.ResponseWriter, req *http.Request) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	item := req.URL.Query().Get("item")
+
+	if _, ok := db.data[item]; !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+
+	delete(db.data, item)
+
+	fmt.Fprintf(w, "dropped %s\n", item)
+}
+
+func main() {
+	db := database{
+		data: map[string]dollars{
+			"shoes": 50,
+			"socks": 5,
+		},
+	}
+
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/fetch", db.fetch)
+	http.HandleFunc("/create", db.create)
+	http.HandleFunc("/update", db.update)
+	http.HandleFunc("/drop", db.drop)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
--- Interfaces/store/store.go
+++ Interfaces/store/store.go
@@ -1,113 +1,113 @@
 package main
 
 // This solution has a concurrency problem which will be fixed later
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"strconv"
-)
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"strconv"
+// )
 
-type dollars float32 // Never use float type for money, use a package
+// type dollars float32 // Never use float type for money, use a package
 
-func (d dollars) String() string {
-	return fmt.Sprintf("$%.2f", d)
-}
+// func (d dollars) String() string {
+// 	return fmt.Sprintf("$%.2f", d)
+// }
 
-// Fake database
-type database map[string]dollars
+// // Fake database
+// type database map[string]dollars
 
-// // add the handlers
-func (db database) list(w http.ResponseWriter, req *http.Request) {
-	for item, price := range db {
-		fmt.Fprintf(w, "%s: %s\n", item, price)
-	}
-}
+// // // add the handlers
+// func (db database) list(w http.ResponseWriter, req *http.Request) {
+// 	for item, price := range db {
+// 		fmt.Fprintf(w, "%s: %s\n", item, price)
+// 	}
+// }
 
-func (db database) fetch(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item")
+// func (db database) fetch(w http.ResponseWriter, req *http.Request) {
+// 	item := req.URL.Query().Get("item")
 
-	if _, ok := db[item]; !ok {
-		msg := fmt.Sprintf("no such item: %q", item)
-		http.Error(w, msg, http.StatusNotFound) //404
-		return
-	}
+// 	if _, ok := db[item]; !ok {
+// 		msg := fmt.Sprintf("no such item: %q", item)
+// 		http.Error(w, msg, http.StatusNotFound) //404
+// 		return
+// 	}
 
-	fmt.Fprintf(w, "item %s has price %s\n", item, db[item])
-}
+// 	fmt.Fprintf(w, "item %s has price %s\n", item, db[item])
+// }
 
-func (db database) create(w http.ResponseWriter, req *http.Request) {
+// func (db database) create(w http.ResponseWriter, req *http.Request) {
 
-	item := req.URL.Query().Get("item")
-	price := req.URL.Query().Get("price")
+// 	item := req.URL.Query().Get("item")
+// 	price := req.URL.Query().Get("price")
 
-	if _, ok := db[item]; ok {
-		msg := fmt.Sprintf("duplicate item: %q", item)
-		http.Error(w, msg, http.StatusBadRequest) // 400
-		return
-	}
+// 	if _, ok := db[item]; ok {
+// 		msg := fmt.Sprintf("duplicate item: %q", item)
+// 		http.Error(w, msg, http.StatusBadRequest) // 400
+// 		return
+// 	}
 
-	p, err := strconv.ParseFloat(price, 32)
+// 	p, err := strconv.ParseFloat(price, 32)
 
-	if err != nil {
-		msg := fmt.Sprintf("invalid price: %q", price)
-		http.Error(w, msg, http.StatusBadRequest) // 400
-		return
-	}
+// 	if err != nil {
+// 		msg := fmt.Sprintf("invalid price: %q", price)
+// 		http.Error(w, msg, http.StatusBadRequest) // 400
+// 		return
+// 	}
 
-	db[item] = dollars(p)
+// 	db[item] = dollars(p)
 
-	fmt.Fprintf(w, "added %s with price %s\n", item, db[item])
-}
+// 	fmt.Fprintf(w, "added %s with price %s\n", item, db[item])
+// }
 
-func (db database) update(w http.ResponseWriter, req *http.Request) {
+// func (db database) update(w http.ResponseWriter, req *http.Request) {
 
-	item := req.URL.Query().Get("item")
-	price := req.URL.Query().Get("price")
+// 	item := req.URL.Query().Get("item")
+// 	price := req.URL.Query().Get("price")
 
-	if _, ok := db[item]; !ok {
-		msg := fmt.Sprintf("no such item: %q", item)
-		http.Error(w, msg, http.StatusNotFound) // 404
-		return
-	}
+// 	if _, ok := db[item]; !ok {
+// 		msg := fmt.Sprintf("no such item: %q", item)
+// 		http.Error(w, msg, http.StatusNotFound) // 404
+// 		return
+// 	}
 
-	p, err := strconv.ParseFloat(price, 32)
+// 	p, err := strconv.ParseFloat(price, 32)
 
-	if err != nil {
-		msg := fmt.Sprintf("invalid price: %q", price)
-		http.Error(w, msg, http.StatusBadRequest) // 400
-		return
-	}
+// 	if err != nil {
+// 		msg := fmt.Sprintf("invalid price: %q", price)
+// 		http.Error(w, msg, http.StatusBadRequest) // 400
+// 		return
+// 	}
 
-	db[item] = dollars(p)
+// 	db[item] = dollars(p)
 
-	fmt.Fprintf(w, "new price %s for price %s\n", db[item], item)
-}
+// 	fmt.Fprintf(w, "new price %s for price %s\n", db[item], item)
+// }
 
-func (db database) drop(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item")
+// func (db database) drop(w http.ResponseWriter, req *http.Request) {
+// 	item := req.URL.Query().Get("item")
 
-	if _, ok := db[item]; !ok {
-		msg := fmt.Sprintf("no such item: %q", item)
-		http.Error(w, msg, http.StatusNotFound) //404
-		return
-	}
+// 	if _, ok := db[item]; !ok {
+// 		msg := fmt.Sprintf("no such item: %q", item)
+// 		http.Error(w, msg, http.StatusNotFound) //404
+// 		return
+// 	}
 
-	delete(db, item)
-	fmt.Fprintf(w, "dropped %s\n", item)
-}
+// 	delete(db, item)
+// 	fmt.Fprintf(w, "dropped %s\n", item)
+// }
 
-func main() {
-	db := database{
-		"shoes": 50,
-		"socks": 5,
-	}
+// func main() {
+// 	db := database{
+// 		"shoes": 50,
+// 		"socks": 5,
+// 	}
 
-	http.HandleFunc("/list", db.list)
-	http.HandleFunc("/fetch", db.fetch)
-	http.HandleFunc("/create", db.create)
-	http.HandleFunc("/update", db.update)
-	http.HandleFunc("/drop", db.drop)
+// 	http.HandleFunc("/list", db.list)
+// 	http.HandleFunc("/fetch", db.fetch)
+// 	http.HandleFunc("/create", db.create)
+// 	http.HandleFunc("/update", db.update)
+// 	http.HandleFunc("/drop", db.drop)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+// }
