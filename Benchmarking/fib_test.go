13f8c4268f9f88f2d628a4701d657214039e4372
IfedayoAwe <olalekanawe99@gmail.com>
2022-12-26 17:04:11 +0100

    bib_test

---
+++ Benchmarking/fib_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+)
+
+func Fib(n int, r bool) int {
+	switch n {
+	case 0:
+		return 0
+	case 1:
+		return 1
+
+	default:
+		if r {
+			return Fib(n-1, true) + Fib(n-2, true)
+		}
+
+		a, b := 0, 1
+
+		for i := 1; i < n; i++ {
+			a, b = b, a+b
+
+		}
+
+		return b
+	}
+}
+
+func BenchmarkFib20T(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Fib(20, true)
+	}
+}
+
+func BenchmarkFib20F(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Fib(20, false)
+	}
+}
