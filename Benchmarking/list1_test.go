ccf0745ae2d80eb0baff31c3682f8942bc10b239
IfedayoAwe <olalekanawe99@gmail.com>
2022-12-27 03:03:05 +0100

    list1

---
+++ Benchmarking/list1_test.go
@@ -0,0 +1,62 @@
+package main
+
+import "testing"
+
+type node struct {
+	v int
+	t *node
+}
+
+func insert(i int, n *node) *node {
+	t := &node{i, nil}
+	if n != nil {
+		n.t = t
+	}
+	return t
+}
+
+func mkList(n int) *node {
+	var h, t *node
+	h = insert(0, h)
+	t = insert(1, h)
+	for i := 2; i < n; i++ {
+		t = insert(i, t)
+	}
+	return h
+}
+
+func sumList(n *node) (i int) {
+	for h := n; h != nil; h = h.t {
+		i += h.v
+	}
+	return
+}
+
+func mkSlice(n int) []int {
+	s := make([]int, n)
+	for i := 0; i < n; i++ {
+		s[i] = i
+	}
+	return s
+}
+
+func sumSlice(s []int) (i int) {
+	for _, v := range s {
+		i += v
+	}
+	return i
+}
+
+func BenchmarkList(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		l := mkList(1200)
+		sumList(l)
+	}
+}
+
+func BenchmarkSlice(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		l := mkSlice(1200)
+		sumSlice(l)
+	}
+}
