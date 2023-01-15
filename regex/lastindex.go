bbd91032b060cc3ad7c48275e4767a2aeb451663
IfedayoAwe <olalekanawe99@gmail.com>
2023-01-15 16:24:53 +0100

    regex2

--- regex/lastindex.go
+++ regex/lastindex.go
@@ -1,30 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"strconv"
-	"strings"
-)
+// import (
+// 	"fmt"
+// 	"runtime"
+// 	"strconv"
+// 	"strings"
+// )
 
-func B() string {
-	_, file, line, _ := runtime.Caller(1)
+// func B() string {
+// 	_, file, line, _ := runtime.Caller(1)
 
-	fmt.Println(file)
+// 	fmt.Println(file)
 
-	fmt.Println(line)
+// 	fmt.Println(line)
 
-	idx := strings.LastIndexByte(file, '/')
+// 	idx := strings.LastIndexByte(file, '/')
 
-	fmt.Println(idx)
+// 	fmt.Println(idx)
 
-	return "=> " + file[idx+1:] + ":" + strconv.Itoa(line)
-}
+// 	return "=> " + file[idx+1:] + ":" + strconv.Itoa(line)
+// }
 
-func A() string {
-	return B()
-}
+// func A() string {
+// 	return B()
+// }
 
-func main() {
-	fmt.Println(A())
-}
+// func main() {
+// 	fmt.Println(A())
+// }
---
+++ regex/regex1.go
@@ -0,0 +1,20 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"regexp"
+// )
+
+// func main() {
+// 	te := "aba abba abbba"
+// 	re := regexp.MustCompile(`b+`)
+// 	mm := re.FindAllString(te, -1)
+// 	id := re.FindAllStringIndex(te, -1)
+
+// 	fmt.Println(mm)
+// 	fmt.Println(id)
+
+// 	for _, d := range id {
+// 		fmt.Println(te[d[0]:d[1]])
+// 	}
+// }
